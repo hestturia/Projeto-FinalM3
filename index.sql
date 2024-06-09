c04cf1eea7458a642eaa94912e85aa020cb40381
Phelipe Cezario <phelipesamuel123@gmail.com>
2024-06-09 02:23:49 -0300

    Análise: Metas completas por Usuário

--- index.sql
+++ index.sql
@@ -169,3 +169,9 @@ SELECT app_site, COUNT(*) as total_ocorrencias
 FROM Distracoes
 GROUP BY app_site
 ORDER BY total_ocorrencias DESC;
+
+--Análise: Metas completas por Usuário
+SELECT usuario_id, COUNT(*) as metas_completas
+FROM Metas
+WHERE status = 'completo'
+GROUP BY usuario_id;
