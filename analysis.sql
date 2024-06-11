f893240532d269413773503abe5b2eb81f24fdfc
JProgramador <pso.joao18@gmail.com>
2024-06-11 15:36:15 -0300

    add: analise p o arquivo 1

--- analysis.sql
+++ analysis.sql
@@ -32,3 +32,9 @@ GROUP BY usuario_id;
 SELECT status, COUNT(*) as total_metas
 FROM Metas
 GROUP BY status;
+
+--joao miguel
+--Média de tempo gasto por Matéria
+SELECT materia, AVG(tempo_gasto) as media_tempo_gasto
+FROM SessaoEstudos
+GROUP BY materia;
