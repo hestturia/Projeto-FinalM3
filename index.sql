ad3dbe490393d22c486491d4776290ae4855da87
JProgramador <pso.joao18@gmail.com>
2024-06-11 15:34:47 -0300

    analise: media de tempo gasto por materia

--- index.sql
+++ index.sql
@@ -225,3 +225,8 @@ GROUP BY usuario_id;
 SELECT status, COUNT(*) as total_metas
 FROM Metas
 GROUP BY status;
+
+--Média de tempo gasto por Matéria
+SELECT materia, AVG(tempo_gasto) as media_tempo_gasto
+FROM SessaoEstudos
+GROUP BY materia;
